1a2457d757ab95bdc13c7e78cf88f3e95759a7cb
Denis Leclair <denis@theleclairs.ca>
2024-06-02 03:35:34 +0100

    fixed the hrtimer crash on init

--- drivers/ads1015/ads1015-sampler.c
+++ drivers/ads1015/ads1015-sampler.c
@@ -13,17 +13,19 @@ enum hrtimer_restart timer_event(struct hrtimer *t) {
 	if (skips > 0) {
 		printk("skipped %ld intervals.", skips);
 	}
-	return HRTIMER_NORESTART;
+	return HRTIMER_RESTART;
 }
 
 
 void ads1015_sampler_init(void) {
 	printk("in ads1015_sampler_init()\n");
-	ads1015_timer.function = &timer_event;
 	hrtimer_init(&ads1015_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	ads1015_timer.function = &timer_event;
 
 	interval = ktime_set(2, 0);
+	printk("interval = %lld\n", interval);
 	hrtimer_start(&ads1015_timer, interval, HRTIMER_MODE_REL);
+	pr_info("ads1015_samplier_init() complete.\n");
 }
 
 
