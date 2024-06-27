fea01142c6fde23b4fd3d6716af5eaad4b724631
Denis Leclair <denis@theleclairs.ca>
2024-06-27 02:56:49 +0100

    increased i2c bus speed to 1M

--- os/boot/firmware/test.sh
+++ os/boot/firmware/test.sh
@@ -1,4 +1,7 @@
 #$!/bin/bash
+
+echo 0x$(xxd /sys/class/i2c-adapter/i2c-1/of_node/clock-frequency | cut -f 2,3 -d ' ' --output-delimiter='') | xargs printf "%d\n"
+
 i2cdetect -y 1
 for ((i = 0; i < 10; i++)); do cat /sys/bus/iio/devices/iio:device0/in_voltage0_raw; done;
 
